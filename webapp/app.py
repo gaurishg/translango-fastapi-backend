4a40de74f796908257a9e30bc53d2c0104477b3c
Gaurish Gangwar <gaurishgangwar@gmail.com>
2022-12-07 21:53:57 +0900

    Removed unnecessary code

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,5 @@
 #app.py
 from flask import Flask, flash, request, redirect, url_for, render_template, jsonify
-import urllib.request
 import os
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
@@ -13,15 +12,11 @@ import sys
 current_path = sys.path[0]
 sys.path.append(os.path.abspath(os.path.join(current_path, '..')))
 sys.path.append(os.path.abspath(os.path.join(current_path, '..', 'yolov7')))
-print(sys.path)
-from yolov7.translango import translango_detect
+# print(sys.path)
+import yolov7.translango
 
 app = Flask(__name__)
- 
-UPLOAD_FOLDER = 'static/uploads/'
- 
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
  
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
@@ -48,10 +43,7 @@ def upload_image():
         flash('No image selected for uploading')
         return redirect(request.url)
     if file and allowed_file(file.filename):
-        print("PRINTING the file")
-        print(type(array))
-        filename = secure_filename(file.filename)
-        detections = translango_detect(array)
+        detections = yolov7.translango.translango_detect(array)
         complete_response = {
             'error': 'no',
             'detections': detections
@@ -69,11 +61,6 @@ def upload_image():
         }
         # return redirect(request.url)
         return json.dumps(error_response)
- 
-@app.route('/display/<filename>')
-def display_image(filename):
-    #print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
- 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
