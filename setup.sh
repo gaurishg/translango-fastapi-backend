3c10308b8bc05013e8a28f860181017710eed2dd
Gaurish Gangwar <gaurishgangwar@gmail.com>
2022-12-07 21:53:21 +0900

    Added setup script for the project

--- setup.sh
+++ setup.sh
@@ -4,9 +4,11 @@ sudo apt update
 sudo apt install -y git python3-pip python3-venv zip htop screen libgl1-mesa-glx wget
 python3 -m venv project_env
 source project_env/bin/activate
-pip install -r yolov7/requirements.txt
+pip install -r yolov7/requirements.txt --no-cache-dir
 pip install flask
 cd yolov7
 wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt
+wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7x.pt
+wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7-tiny.pt
 cd ..
 sudo project_env/bin/python webapp/app.py
