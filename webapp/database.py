e30093b3f2a5d4f256572c7e656314296348ec58
Gaurish Gangwar <gaurishgangwar@gmail.com>
2022-12-26 17:03:09 +0000

    Added languages to users

--- webapp/database.py
+++ webapp/database.py
@@ -1,9 +1,10 @@
 from sqlmodel import Field # type: ignore
+from sqlmodel import Relationship # type: ignore
 # from sqlmodel import CHAR
 from sqlmodel import SQLModel, create_engine
 from sqlalchemy.orm import declared_attr
 from sqlmodel.pool import StaticPool
-from typing import Optional
+from typing import Optional, List
 
 engine = create_engine(
     'sqlite:///', 
@@ -12,9 +13,21 @@ engine = create_engine(
     poolclass=StaticPool
     )
 
+
+class UserFavLangs(SQLModel, table=True):
+    username: str = Field(primary_key=True, foreign_key="users.username")
+    iso639_1code: str = Field(primary_key=True, foreign_key="languages.iso639_1code", min_length=2, max_length=2)
+
 class Language(SQLModel, table=True):
     iso639_1code: str = Field(primary_key=True, nullable=False, min_length=2, max_length=2)
     name_in_en: str
+
+    users_with_primary_language: List["UserInDB"] = Relationship(back_populates="primary_language")
+    users_with_favourite_languages: List["UserInDB"] = Relationship(back_populates="favourite_languages", link_model=UserFavLangs)
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return "languages"
 
     def __repr__(self) -> str:
         return f"Language({self.iso639_1code}: {self.name_in_en})"
@@ -25,6 +38,8 @@ class User(SQLModel):
     middlename: Optional[str] = Field(default=None)
     lastname: Optional[str] = Field(default=None)
     email: str = Field(unique=True, nullable=False)
+    primary_lang: str = Field(nullable=False, foreign_key="languages.iso639_1code", min_length=2, max_length=2)
+    
 
 class UserFromFrontend(User):
     password: str
@@ -36,6 +51,13 @@ class UserInDB(User, table=True):
     
     hashed_password: bytes
     salt: bytes
+
+    primary_language: Language = Relationship(back_populates="users_with_primary_language")
+    favourite_languages: List[Language] = Relationship(back_populates="users_with_favourite_languages", link_model=UserFavLangs)
+
+class UserToFrontend(UserInDB):
+    pass
+
 
 if __name__ == '__main__':
     SQLModel.metadata.create_all(bind=engine)
