db3e33db7a633ca9780298fa8fd309a1dffdaa34
Gaurish Gangwar <gaurishgangwar@gmail.com>
2022-12-28 09:54:35 +0000

    Added AWS (using boto3) functions to upload to S3 and get presigned urls

---
+++ webapp/aws_api_helpers.py
@@ -0,0 +1,55 @@
+import pydantic
+import boto3
+from PIL import Image
+import io
+import hashlib
+
+BUCKET_NAME = 'translango'
+DEFAULT_EXPIRATION_TIME_FOR_PRESIGNED_URLS = 1 * 60 # 1 minute (or 60 seconds)
+IMG_FORMAT = 'jpeg'
+
+s3_resource = boto3.resource('s3')
+translango_bucket = s3_resource.Bucket(BUCKET_NAME)
+
+class S3Bucket(pydantic.BaseModel):
+    name: str
+
+    class Config:
+        orm_mode = True
+
+class S3Object(pydantic.BaseModel):
+    bucket_name: str
+    key: str = pydantic.Field(description="name of the object uploaded in s3")
+
+    class Config:
+        orm_mode = True
+
+def upload_PIL_Image_to_s3(image: Image.Image) -> S3Object:
+    name = hashlib.sha512(image.tobytes())
+    # print(f"hash of the image: {name.name=}, {name.hexdigest()=}, {len(name.hexdigest())=} {name.digest_size=}")
+    name = name.hexdigest() + f".{IMG_FORMAT}"
+    in_mem_file = io.BytesIO()
+    image.save(in_mem_file, format=IMG_FORMAT)
+    in_mem_file.seek(0)
+    return S3Object.from_orm(translango_bucket.put_object(Key=name, Body=in_mem_file))
+
+def upload_image_fileobj_to_s3(image: bytes) -> S3Object:
+    name = hashlib.sha512(image).hexdigest() + '.' + IMG_FORMAT
+    return S3Object.from_orm(translango_bucket.put_object(Key=name, Body=image))
+
+
+def create_presigned_url(object_name, expiration=DEFAULT_EXPIRATION_TIME_FOR_PRESIGNED_URLS) -> pydantic.FileUrl:
+    """Generate a presigned URL to share an S3 object
+
+    :param bucket_name: string
+    :param object_name: string
+    :param expiration: Time in seconds for the presigned URL to remain valid
+    :return: Presigned URL as string. If error, returns None.
+    """
+    response = s3_resource.meta.client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': BUCKET_NAME,
+                                                            'Key': object_name},
+                                                    ExpiresIn=expiration)
+
+    # The response contains the presigned URL
+    return pydantic.FileUrl(url=response, scheme='https')
